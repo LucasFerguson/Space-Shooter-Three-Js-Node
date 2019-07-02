a9b50be985279b1361b2e90a31db33b366e3f8ea
Lucas <lucasferguson16@gmail.com>
2019-07-02 15:36:39 -0500

    ADDING TEXT TYPINGS
    Added text typings for three js.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Space Shooter Three Js Node
  My 4th attempt at creating a space shooter 
+
+Three.Js Github
+https://github.com/mrdoob/three.js/
---
+++ public/js/game.js
@@ -0,0 +1,65 @@
+var scene, renderer;
+var geometry, material, mesh;
+var camera;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
+}
+
+
+
+
+
+
+// mousemove
+window.addEventListener('mousemove', onDocumentMouseMove, false);
+
+
+function onDocumentMouseMove(event) {
+    event.preventDefault();
+    mouse.x = event.clientX / windowWidth;
+    mouse.y = -event.clientY / windowHeight;
+}
+
+
+// onWindowResize
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    renderer.setSize(windowWidth, windowHeight);
+    camera.aspect = windowWidth / windowHeight;
+    camera.updateProjectionMatrix();
+}
